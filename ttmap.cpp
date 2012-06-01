e4733fea7c27ae9828e2faeeacc1ac913dc5455c
HuangCongyu <huangcongyu2006@gmail.com>
2012-06-01 11:33:49 +0800

    点击选中目标集群

--- ttmap.cpp
+++ ttmap.cpp
@@ -54,12 +54,12 @@ void TTMap::timeOutHandler()
 {
     targetGenerator->go(1);
 
-    qDebug() << "Current states:";
-    std::vector<State> states = targetGenerator->getCurrentStates();
-    std::vector<State>::iterator iter;
-    for (iter = states.begin(); iter != states.end(); ++iter) {
-        (*iter).print();
-    }
+//    qDebug() << "Current states:";
+//    std::vector<State> states = targetGenerator->getCurrentStates();
+//    std::vector<State>::iterator iter;
+//    for (iter = states.begin(); iter != states.end(); ++iter) {
+//        (*iter).print();
+//    }
 
     update();
 }
@@ -73,4 +73,45 @@ void TTMap::paintEvent(QPaintEvent */*event*/)
     while (i.hasNext()) {
         i.next()->draw(painter, width(), height());
     }
+}
+
+void TTMap::mouseReleaseEvent(QMouseEvent *e)
+{
+    qDebug() << "Button click" << "(" << e->x() << "," << e->y() << ")";
+    TargetGroup *g = getClickedTargetGroup(e->x(), e->y());
+    if (g) qDebug() << "click group:" << g->getID();
+}
+
+TargetGroup* TTMap::getClickedTargetGroup(int x, int y)
+{
+    std::vector<TargetGroup*> *groups = targetGenerator->getGroups();
+    std::vector<TargetGroup*>::iterator iter;
+    TargetGroup *tg;
+    for (iter = groups -> begin(); iter != groups -> end(); ++iter) {
+        tg = (*iter);
+        if (isTargetGroupClicked(tg, x, y)) {
+            return tg;
+        }
+    }
+    return NULL;
+}
+
+bool TTMap::isTargetGroupClicked(TargetGroup *grp, int x, int y)
+{
+    if (grp == NULL) return false;
+    SingleTarget *t;
+    State *s;
+    for (int i = 0; i < grp->getTargetCount(); ++i) {
+        t = grp->getTarget(i);
+        for (int j = 0; j < t->getStateCount(); ++j) {
+            s = t->getState(j);
+            float xx, yy;
+            xx = width() * (s -> getPositionX()) / TTMap::WIDTH;
+            yy = height() - height() * ( s -> getPositionY() ) / TTMap::HEIGHT;
+            if (qAbs(xx - x) < 5 && qAbs(yy - y) < 5) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
--- ttmap.h
+++ ttmap.h
@@ -6,6 +6,7 @@
 #include "datasource/targetgenerator.h"
 #include "view/targetgroupview.h"
 #include <QTimer>
+#include <QMouseEvent>
 
 /*
  * 地图
@@ -17,6 +18,7 @@ class TTMap : public QWidget
 public:
     explicit TTMap(QWidget *parent = 0);
     void paintEvent(QPaintEvent *);
+    void mouseReleaseEvent(QMouseEvent *);
 
     // 所模拟的区域是10000 * 10000大小的区域
     const static int WIDTH = 10000;
@@ -36,6 +38,15 @@ private:
     QList<TargetGroupView*> views;
 
     QTimer timer;       // 定时器
+
+    //
+    // 获取被点击的集群
+    //
+    TargetGroup* getClickedTargetGroup(int x, int y);
+    //
+    // 判断集群是否被点击
+    //
+    bool isTargetGroupClicked(TargetGroup *grp, int x, int y);
 };
 
 #endif // TTMAP_H
